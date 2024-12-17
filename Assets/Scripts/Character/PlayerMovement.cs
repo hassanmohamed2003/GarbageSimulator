57aa31900aba88d62cabbc5d6ea762e5903d0c13
Hassan Mohamed <s1136140@student.hsleiden.nl>
2024-12-17 18:44:10 +0100

    feat: player able to move + tile map example

---
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float _playerSpeed = 10;
+    PlayerInput _Input;
+    Vector2 _Movement;
+    Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _Input = new PlayerInput();
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable()
+    {
+        _Input.Enable();
+
+        _Input.Gameplay.Movement.performed += onMovement;
+        _Input.Gameplay.Movement.canceled += onMovement;
+    }
+
+    private void OnDisable()
+    {
+        _Input.Disable();
+    }
+
+    private void onMovement(InputAction.CallbackContext context)
+    {
+        _Movement = context.ReadValue<Vector2>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = _Movement * _playerSpeed;
+    }
+}
